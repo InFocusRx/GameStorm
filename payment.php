a2bfea99fac5bfa6c76e6652886a1dbc94f1fcd5
Nikita Rezvan <nikitarezvan@gmail.com>
2018-06-13 00:25:16 +0300

    Payment page was created

---
+++ payment.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Success!</title>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+
+  <script>
+  document.onload = setTimeout(function () { window.location.replace('game-list.php'); }, 7000);
+  </script>
+</head>
+<body style="margin-top: 17.5%; text-align: center; font-size: 32px;">
+  <i style="color: lime; font-size: 136px;" class="fas fa-check"></i><br><br>
+  <img src="img/logo.png" alt="Logo" width="10%">
+  <h1>Thank you for purchase</h1>
+  <small>loading...</small>
+  
+</body>
+</html>
